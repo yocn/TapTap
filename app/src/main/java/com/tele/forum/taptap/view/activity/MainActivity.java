508772b415a5c64cb4cc585f8380b3acd8ada182
yocn <539654997@qq.com>
2017-01-06 17:42:43 +0800

    first init:

---
+++ app/src/main/java/com/tele/forum/taptap/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tele.forum.taptap.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tele.forum.taptap.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
